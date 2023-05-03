1d73d7394024af49ae2118107da2b7b91d4a0089
Steven <codebusiness2014@gmail.com>
2023-05-03 12:12:20 -0500

    New files for new routes

--- chirps/src/pages/index.tsx
+++ chirps/src/pages/index.tsx
@@ -11,6 +11,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -92,10 +93,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1">
-          <span className="font-bold">{`@${author?.username}`}</span>
-          <span className="font-thin">
-            {` · ${dayjs(post.createdAt).fromNow(true)}`}
-          </span>
+          <Link href={`/@${author?.username}`}>
+            <span className="font-bold">{`@${author?.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">
+              {` · ${dayjs(post.createdAt).fromNow(true)}`}
+            </span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
